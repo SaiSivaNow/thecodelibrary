d229629c9f1e2e7dff745a6eb512320cf219e9f5
yanreddy <yanreddy@zetaglobal.com>
2023-04-22 17:48:01 +0530

    refactor changes

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 
 app.get('/videoplayer', (req, res) => {
 	const range = req.headers.range;
-	const chunkSize = 1 * 1e6;
+	const chunkSize = 1e6;
 
 	const params = {
 	  Bucket: bucketName,
@@ -50,33 +50,11 @@ app.get('/videoplayer', (req, res) => {
 	  s3Stream.on('error', (err) => {
 		console.error(err);
 		res.sendStatus(500);
-		return;
 	  });
 
 	  s3Stream.pipe(res);
 	});
   });
-
-	const range = req.headers.range
-	const videoPath = './video.mov';
-	const videoSize = fs.statSync(videoPath).size
-	const chunkSize = 1e6;
-	const start = Number(range.replace(/\D/g, ""))
-	const end = Math.min(start + chunkSize, videoSize - 1)
-	const contentLength = end - start + 1;
-	const headers = {
-		"Content-Range": `bytes ${start}-${end}/${videoSize}`,
-		"Accept-Ranges": "bytes",
-		"Content-Length": contentLength,
-		"Content-Type": "video/mp4"
-	}
-	res.writeHead(206, headers)
-	const stream = fs.createReadStream(videoPath, {
-		start,
-		end
-	})
-	stream.pipe(res)
-})
 
 app.get ('/auth', async (req, res) => {
 	try {
