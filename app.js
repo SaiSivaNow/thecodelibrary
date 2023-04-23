93ccd8beaa57207c3d46cdf1f035d854ce4a6a27
yanreddy <yanreddy@zetaglobal.com>
2023-04-23 07:57:18 +0530

    Authenticating with oauth and jwt token with in-memory users

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const express = require('express');
-const app = express();
 const dotenv = require("dotenv")
-dotenv.config()
-const utils = require ('./utils');
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const bucketName = 'thecodelibrary-lite';
 const objectKey = 'programming.mov';
+const DATA = [{email:"govardhan.yannam@gmail.com", provider:"google"}]
 
-app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
-})
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(passport.initialize());
+app.use(session({
+	resave: false,
+	saveUninitialized: true,
+	secret: 'secret'
+}));
+dotenv.config()
 
-app.get('/videoplayer', (req, res) => {
+
+app.get('/videoplayer', passport.authenticate('jwt', { session: false }),
+	(req, res) => {
 	const range = req.headers.range;
 	const chunkSize = 1e6;
 
@@ -56,25 +68,126 @@ app.get('/videoplayer', (req, res) => {
 	});
   });
 
-app.get ('/auth', async (req, res) => {
-	try {
-		res.redirect (utils.request_get_auth_code_url);
-	} catch (error) {
-		res.sendStatus (500);
-		console.log (error.message);
+const jwt = require('jsonwebtoken')
+var JwtStrategy = require('passport-jwt').Strategy
+var ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {}
+opts.jwtFromRequest = function(req) {
+	var token = null;
+	if (req && req.cookies)
+	{
+		token = req.cookies['jwt'];
 	}
+	return token;
+};
+opts.secretOrKey = 'secret';
+
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+	console.log("JWT BASED  VALIDATION GETTING CALLED")
+	console.log("JWT", jwt_payload)
+	if (CheckUser(jwt_payload.data)) {
+		return done(null, jwt_payload.data)
+	} else {
+		// user account doesnt exists in the DATA
+		return done(null, false);
+	}
+}));
+
+passport.use(new GoogleStrategy({
+		clientID: process.env.CLIENT_APP_ID,
+		clientSecret: process.env.CLIENT_APP_SECRET,
+		callbackURL: `http://localhost:3000${process.env.REDIRECT_URI}`
+	},
+	function(accessToken, refreshToken, profile, cb) {
+		//console.log(accessToken, refreshToken, profile)
+		console.log("GOOGLE BASED OAUTH VALIDATION GETTING CALLED")
+		return cb(null, profile)
+	}
+));
+
+passport.serializeUser(function(user, cb) {
+	console.log('I should have jack ')
+	cb(null, user);
 });
 
-app.get (process.env.REDIRECT_URI, async (req, res) => {
-	const authorization_token = req.query;
-	console.log ({auth_server_response: authorization_token});
-	try {
-		res.sendFile(__dirname + '/index.html');
-	} catch (error) {
-		console.log (error.message);
-		res.sendStatus (500);
-	}
+passport.deserializeUser(function(obj, cb) {
+	console.log('I wont have jack shit')
+	cb(null, obj);
 });
 
-app.listen(3000);
+app.get('/', (req, res)=>{
+	res.sendFile('home.html', {root: __dirname+'/public'})
+})
 
+app.get('/login', (req, res)=>{
+	res.sendFile('login.html', {root: __dirname+'/public'})
+})
+
+app.get('/auth/email', (req, res)=>{
+	res.sendFile('login_form.html',  {root: __dirname+'/public'})
+})
+
+app.get('/auth/google',  passport.authenticate('google', { scope: ['profile','email'] }))
+
+app.post('/auth/email', (req, res)=>{
+
+	if(CheckUser(req.body))
+	{
+		let token =    jwt.sign({
+			data: req.body
+		}, 'secret', { expiresIn: '1h' });
+		res.cookie('jwt', token)
+		res.send(`Log in success ${req.body.email}`)
+	}else{
+		res.send('Invalid login credentials')
+	}
+})
+
+app.get('/profile', passport.authenticate('jwt', { session: false }) ,(req,res)=>{
+	res.send(`THIS IS UR PROFILE MAAANNNN ${req.user.email}`)
+})
+
+app.get(process.env.REDIRECT_URI, passport.authenticate('google'),(req, res)=>{
+	console.log('redirected', req.user)
+	let user = {
+		displayName: req.user.displayName,
+		name: req.user.name.givenName,
+		email: req.user._json.email,
+		provider: req.user.provider }
+	console.log(user)
+
+	FindOrCreate(user)
+	let token = jwt.sign({
+		data: user
+	}, 'secret', { expiresIn: '4h' });
+	res.cookie('jwt', token)
+	res.redirect('/')
+})
+
+function FindOrCreate(user){
+	if(CheckUser(user)){  // if user exists then return user
+		return user
+	}else{
+		DATA.push(user) // else create a new user
+	}
+}
+function CheckUser(input){
+	console.log(DATA)
+	console.log(input)
+
+	for (var i in DATA) {
+		if(input.email===DATA[i].email && (input.password===DATA[i].password || DATA[i].provider===input.provider))
+		{
+			console.log('User found in DATA')
+			return true
+		}
+		else
+			null
+		//console.log('no match')
+	}
+	return false
+}
+const port = process.env.PORT || 3000
+app.listen( port, ()=>{
+	console.log(`Sever ARG0 listening on port ${port}`)
+})
