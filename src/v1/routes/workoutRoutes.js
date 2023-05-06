5c7c8fc722a7734b10df5b2814572ac77f424d79
rakesh <rakeshvarma8106513311@gmail.com>
2023-05-06 21:06:35 +0530

    future use case

---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const workoutController = require("../../controllers/workoutController");
+// *** ADD ***
+const recordController = require("../../controllers/recordController");
+
+const router = express.Router();
+
+router.get("/", workoutController.getAllWorkouts);
+
+router.get("/:workoutId", workoutController.getOneWorkout);
+
+// *** ADD ***
+router.get("/:workoutId/records", recordController.getRecordForWorkout);
+
+router.post("/", workoutController.createNewWorkout);
+
+router.patch("/:workoutId", workoutController.updateOneWorkout);
+
+router.delete("/:workoutId", workoutController.deleteOneWorkout);
+
+module.exports = router;
