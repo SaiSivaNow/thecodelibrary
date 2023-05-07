b0daddf0387b5d67fc6e07cd7d1efee08e6c40a3
rakesh <rakeshvarma8106513311@gmail.com>
2023-05-07 11:50:56 +0530

    course catalog api changes

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const express = require('express');
-const passport = require('passport')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bucketName = 'thecodelibrary-lite';
 const objectKey = 'programming.mov';
@@ -12,74 +12,76 @@ const dotenv = require("dotenv");
 const env = process.env.NODE_ENV || 'prod';
 const config = require(`./config/${env}.json`);
 dotenv.config()
-const authRoute=require('./routes/auth')
+const authRoute = require('./routes/auth')
 const paymentRoute = require('./routes/payment')
+const courseRoute = require('./routes/course')
 const port = config.PORT;
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(passport.initialize());
 app.use(session({
-	resave: false,
-	saveUninitialized: true,
-	secret: 'secret'
+    resave: false,
+    saveUninitialized: true,
+    secret: 'secret'
 }));
 app.use("/auth", authRoute)
 app.use("/pay", paymentRoute)
+app.use("/course", courseRoute)
 
 
-app.get('/videoplayer', passport.authenticate('jwt', { session: false }),
-	(req, res) => {
-	const range = req.headers.range;
-	const chunkSize = 1e6;
+app.get('/videoplayer', passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        const range = req.headers.range;
+        const chunkSize = 1e6;
 
-	const params = {
-	  Bucket: bucketName,
-	  Key: objectKey
-	};
+        const params = {
+            Bucket: bucketName,
+            Key: objectKey
+        };
 
-	s3.headObject(params, (err, metadata) => {
-	  if (err) {
-		console.error(err);
-		res.sendStatus(500);
-		return;
-	  }
+        s3.headObject(params, (err, metadata) => {
+            if (err) {
+                console.error(err);
+                res.sendStatus(500);
+                return;
+            }
 
-	  const videoSize = metadata.ContentLength;
-	  const start = Number(range.replace(/\D/g, ''));
-	  const end = Math.min(start + chunkSize, videoSize - 1);
-	  const contentLength = end - start + 1;
-	  const headers = {
-		'Content-Range': `bytes ${start}-${end}/${videoSize}`,
-		'Accept-Ranges': 'bytes',
-		'Content-Length': contentLength,
-		'Content-Type': 'video/mp4',
-	  };
-	  res.writeHead(206, headers);
+            const videoSize = metadata.ContentLength;
+            const start = Number(range.replace(/\D/g, ''));
+            const end = Math.min(start + chunkSize, videoSize - 1);
+            const contentLength = end - start + 1;
+            const headers = {
+                'Content-Range': `bytes ${start}-${end}/${videoSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': contentLength,
+                'Content-Type': 'video/mp4',
+            };
+            res.writeHead(206, headers);
 
-	  const s3Stream = s3.getObject({
-		Bucket: bucketName,
-		Key: objectKey,
-		Range: `bytes=${start}-${end}`
-	  }).createReadStream();
+            const s3Stream = s3.getObject({
+                Bucket: bucketName,
+                Key: objectKey,
+                Range: `bytes=${start}-${end}`
+            }).createReadStream();
 
-	  s3Stream.on('error', (err) => {
-		console.error(err);
-		res.sendStatus(500);
-	  });
+            s3Stream.on('error', (err) => {
+                console.error(err);
+                res.sendStatus(500);
+            });
 
-	  s3Stream.pipe(res);
-	});
-  });
+            s3Stream.pipe(res);
+        });
+    });
 
-app.get('/', (req, res)=>{
-	res.sendFile('home.html', {root: __dirname+'/public'})
+app.get('/', (req, res) => {
+    res.sendFile('home.html', {root: __dirname + '/public'})
 })
 
-app.get('/profile', passport.authenticate('jwt', { session: false }) ,(req,res)=>{
-	res.send(`THIS IS UR PROFILE MAAANNNN ${req.user.email}`)
+app.get('/profile', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.send(`THIS IS UR PROFILE MAAANNNN ${req.user.email}`)
 })
 
-app.listen( port, ()=> {
-	console.log(`Sever TheCodeLibrary listening on port ${port}`)
+app.listen(port, () => {
+    console.log(`Sever TheCodeLibrary listening on port ${port}`)
 })
